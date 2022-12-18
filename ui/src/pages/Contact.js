f1fe2d0b5d7060efe88286f57af592a164c2d57a
gangiCentra <jamesgangi@centramarketing.co>
2022-12-18 09:07:24 -0500

    kyles changes to contact page

--- ui/src/pages/Contact.js
+++ ui/src/pages/Contact.js
@@ -1,12 +1,12 @@
 import "../styles/Contact.css";
 export function Contact(props) {
     return (
-        <div>
-            <form>
-                <input class="contact-input" type="text" value="First Name" />
-                <input class="contact-input" type="text" value="Last Name" />
-                <input class="contact-input" type="text" value="Email" />
-                <input class="contact-input" type="text" value="Message" />
+        <div class="input-div">
+            <form class="input-form">
+                <input class="contact-input" id="name" type="text" value="First Name" />
+                <input class="contact-input" id="name" ype="text" value="Last Name" />
+                <input class="contact-input" id="email"type="text" value="Email" />
+                <input class="contact-input" id="message" type="text" value="Message" />
             </form>
         </div>
     );
