48675604c623a8fb144edf3437714b0b5f535994
gangiCentra <jamesgangi@centramarketing.co>
2022-12-18 12:17:07 -0500

    some work on the contact page and form

--- ui/src/pages/Contact.js
+++ ui/src/pages/Contact.js
@@ -1,13 +1,10 @@
 import "../styles/Contact.css";
+import { ContactForm } from "../components/ContactForm.js";
+
 export function Contact(props) {
     return (
         <div class="input-div">
-            <form class="input-form">
-                <input class="contact-input" id="name" type="text" value="First Name" />
-                <input class="contact-input" id="name" ype="text" value="Last Name" />
-                <input class="contact-input" id="email"type="text" value="Email" />
-                <input class="contact-input" id="message" type="text" value="Message" />
-            </form>
+            <ContactForm/>
         </div>
     );
 }
--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,7 +1,33 @@
+import '../styles/Home.css';
+
 export function Home(props) {
   return (
-    <div>
-      <h1>biach this is my home</h1>
+    <div class="container">
+      <div class="top-container">
+        <div class="home-page-left-top">
+         <p>this is where rotataing intro text will go</p>
+        </div>
+        <div class="home-page-right-top">
+          <p>this is where intro paragraph text will go</p>
+        </div>
+      </div>
+      <div class="middle-container">
+        <div class="home-page-left-middle">
+         <p>basic info</p>
+        </div>
+        <div class="home-page-right-middle">
+          <p>paired graphic with info</p>
+        </div>
+      </div>
+      <div class="bottom-container">
+        <div class="home-page-left-bottom">
+         <p>final convincer</p>
+        </div>
+        <div class="home-page-right-bottom">
+          <p>another grpahic</p>
+        </div>
+      </div>
     </div>
+    
   );
 }
