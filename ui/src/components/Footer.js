8f095124d6734271dcad15efe9e38e5bf305139d
gangiCentra <jamesgangi@centramarketing.co>
2023-01-22 12:55:50 -0500

    changed style

--- ui/src/components/Footer.js
+++ ui/src/components/Footer.js
@@ -32,26 +32,24 @@ export function Footer(props) {
                         <h4>Contact Us</h4>
                     </div>
                 </Link>
-
-                <div class="social-container">
-                    <a href="https://www.youtube.com" className="youtube social">
-                        <FontAwesomeIcon icon={faYoutube} size="2x" />
-                    </a>
-                    <a href="https://www.facebook.com" className="facebook social">
-                        <FontAwesomeIcon icon={faFacebook} size="2x" />
-                    </a>
-                    <a href="https://www.instagram.com" className="instagram social">
-                        <FontAwesomeIcon icon={faInstagram} size="2x" />
-                    </a>
-                    <a href="https://www.linkedin.com" className="linkedin social">
-                        <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
-                    </a>
-                    <a href="https://www.twitter.com" className="twitter social">
-                        <FontAwesomeIcon icon={faTwitter} size="2x" />
-                    </a>
-                </div>
             </div>
-            
+            <div class="social-container">
+                <a href="https://www.youtube.com" className="youtube social">
+                    <FontAwesomeIcon icon={faYoutube} size="2x" />
+                </a>
+                <a href="https://www.facebook.com" className="facebook social">
+                    <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+                <a href="https://www.instagram.com" className="instagram social">
+                    <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+                 <a href="https://www.linkedin.com" className="linkedin social">
+                    <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+                </a>
+                <a href="https://www.twitter.com" className="twitter social">
+                    <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
+            </div>            
         </div>
     );
 }
