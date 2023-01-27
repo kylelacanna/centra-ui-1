009afd387a2d32511d26492ff7277bf19709b061
gangiCentra <jamesgangi@centramarketing.co>
2023-01-27 10:55:16 -0500

    more changes

--- ui/src/pages/About.js
+++ ui/src/pages/About.js
@@ -1,14 +1,25 @@
+import '../styles/About.css';
+
 export function About(props) {
     return (
-        <div>
-            <h2>About Centra</h2>
-            <div>
-                <h3>The Team</h3>
-                <p>Jimmy Gangi</p>
-                <p>Kyle LaCanna</p>
-                <p>Laughton Williams</p>
-                <p>Phil Helms</p>
+        <div class="about-container">
+            <h3>How Centra Came To Be</h3>
+            <div class="about-top-container">
+                <div class="about-image-container">
+                    <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" width="150" alt=""></img>
+                </div>
+                <div class="about-us-text">
+                    <span>Cenra was founded with one mission in mind, to help and assist business in a space where they are new.</span><br/>
+                    <span>Our founders saw a need for advertising Optimization and management in the digital world and we work hard everyday to ensure that our customers have every advantage possible to allow them to succeed in achieving their goals.</span><br/>
+                    <span>In late 2022 we began our journey building our services to meet the high standards we set for ourselves.</span><br/>
+                    <span>Today we are happy to offer assitance to our customers on a number of major digital platforms in order to make certain that our customers recieve added value that otherwise would have been unobtainable.</span>
+                </div>
+                
+               
             </div>
+            <btn class="home-page-btn">
+                Together We Will succeed
+            </btn>
         </div>
     );
 }
--- ui/src/pages/Services.js
+++ ui/src/pages/Services.js
@@ -3,56 +3,50 @@ import '../styles/Services.css';
 export function Services(props) {
     return (
         <div class="parent-container">
-            <div class="top-container">
-                <h1>Ecommerce Marketing Services Tailored for Profit Growth</h1>
-                <caption>Centra Marketing is an ecommerce focused Marketing Agency that specialize in crafting effective campaigns for maximum impact, with a focus on driving business growth and increasing revenue. </caption>
+            <div class="services-top-container">
+                <h1><mark>Ecommerce</mark> Marketing Services Centered for <mark>Profit Growth</mark></h1>
+                <caption>Centra Marketing is an ecommerce focused Marketing Agency that specializes in crafting effective campaigns for maximum impact, with a focus on driving business growth and increasing revenue. </caption>
                 <btn class="contact-us-btn">
                     Get My Free Ecommerce Marketing Strategy
                 </btn>
             </div>
-            <div class="middle-container">
-                <div class="lead-gen-desc">
-                    <p>Ad Management</p>
-                    <p>Ad Optimization</p>
-                    <p>User Feedback</p>
+            <div class="services-middle-container">
+                <div class="all-unique-text">
+                    <h2>No Two Brands are Indentical</h2>
+                    <caption>bullshit</caption>
+                    <br/>
+                    <h2>Discover How Big Your Audience Can Be</h2>
+                    <caption>bullshitbullshitbullshitbullshitbullshitbullshit</caption>
                 </div>
             </div>
-            <div class="bottom-container">
-                <div class="home-page-left-bottom">
-                    <p>palceholder</p>
-                </div>
-                <div class="home-page-right-bottom">
-                    <p>placeholder</p>
-                </div>
-             </div>
-
+            <h1> What We Can Provide To Your Business</h1>
              <div id="services-cards">
+                
                 <div class="card" id="card-1">
                     <div class="card-content">
-                        <h2>Ad Management</h2>
+                        <h2>Ad Management and Optimization</h2>
                         <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" width="150" alt=""></img>
-
-                        <p>what our team will do</p>
+                        <p>Leave your social media advertising in the hands of our professional team, who will take care of posting giving you the freedom to focus on other areas of your business. We provide:</p>           
                         <ul>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
+                            <li>Facebook:</li>
+                            <li>LinkedIn:</li>
+                            <li>Instagram:</li>
+                            <li>bullshit</li>
                         </ul>
                     </div>
                 </div>
                 <div class="card" id="card-1">
                     <div class="card-content">
-                        <h2>Ad Optimization</h2>
+                        <h2>Google PPC</h2>
                         <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" width="150" alt=""></img>
                         <p>
-                            Our expert team will 
+                            You can Entrust our Google Premier Partner marketing experts to construct a custom PPC campaign, such as a Google ads strategy, to accomplish your business objectives.
                         </p>
                         <ul>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
+                            <li>About google ppc</li>
+                            <li>bullshit</li>
+                            <li>bullshit</li>
+                            <li>bullshit</li>
                         </ul>
                     </div>
                 </div>
@@ -61,13 +55,13 @@ export function Services(props) {
                         <h2>User Feedback</h2>                        
                         <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" width="150" alt=""></img>
                         <p>
-                            Our expert team will 
+                        By outsourcing your advertising needs to our team of professionals, we can provide valuable feedback data that will enable your marketing department to create content that resonates with your target audience, ultimately saving you time and resources on research. 
                         </p>
                         <ul>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
+                            <li>bullshit</li>
+                            <li>bullshit</li>
+                            <li>bullshit</li>
+                            <li>bullshit</li>
                         </ul>
                     </div>
                 </div>
