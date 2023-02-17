d8c6021a52a21a30aefc4501c125ed90746d4b72
gangiCentra <jamesgangi@centramarketing.co>
2023-02-17 13:13:00 -0500

    who we help changes

--- ui/src/pages/WhoWeHelp.js
+++ ui/src/pages/WhoWeHelp.js
@@ -8,68 +8,91 @@ export function WhoWeHelp(props){
                 <h1 class="who-we-help-h1">dedicated to <mark>helping businesses</mark> with <br/>an online presence <mark>expand</mark> their reach</h1>
             </div>
             <div class="container-who-we-help">
-               <h2><mark class="white">Highlighting</mark> our values, goals, and <mark class="white">our objectives</mark></h2>
+               <h2><mark>Highlighting</mark> our values, goals, and <mark >our objectives</mark></h2>
                 <div class="icons">
-                    <div>
+                    <div class="icon">
                         <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" alt=""></img>
                         <h1>Our Values</h1>
                         <span>Our team values ongoing learning and development as a key driver of our success. This approach allows us to stay current and relevant in our field, and to tackle new challenges with confidence.</span>
                     </div>
-                    <div>
+                    <div class="icon">
                         <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" alt=""></img>
                         <h1>Setting Goals</h1> 
                         <span>At Centra, we believe that setting ambitious goals is essential to driving progress and fostering a culture of creativity and excellence. By setting clear, challenging objectives and working together as a team to achieve them.</span>
                     </div>
-                    <div>
+                    <div class="icon">
                         <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" alt=""></img>
                         <h1>Grow Your Business</h1>
                         <span>At Centra, our team employs a data-driven approach to help you understand your audience, and to identify the strategies that will be most effective in helping your business grow.</span>
                     </div>
                 </div>
                 <div class="steps-backdrop">
-                    <h1>Our Recipe For Growth</h1>
+                    <h1>Our Recipe For <mark>Growth</mark></h1>
                     <div class="steps">
                         <div class="steps-list">
                             <div class="row">
                                 <div>
-                                    <h2> Friendly Chat</h2>
+                                    <h2><span>1</span> Friendly Chat</h2>
                                     <p>We’ ll start by having a quick phone call talking  about what your challenges are right now, what you’ve tried so far, and if we're the right Ad management marketing agency for you. No hard selling. Promise.</p>
                                 </div>
-                                <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" width="150" alt=""></img>
+                                <div class="hexagon">
+                                    <img src="https://www.northeastern.edu/graduate/blog/wp-content/uploads/2021/02/virtual-group-projects.jpg" width="650" alt="hexagon"></img>
+                                </div>
                             </div>
                             <div class="row">
-                            <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" width="150" alt=""></img>
+                                <div class="hexagon">
+                                    <img src="https://images.pexels.com/photos/1325766/pexels-photo-1325766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width="650" alt="hexagon"></img>
+                                </div>
                                 <div>
-                                    <h2> Team Discussion</h2>
+                                    <h2><span>2</span>   Team Discussion</h2>
                                     <p>After our team has had a chance to speak with you and yours, we will meet privatly and come up with soluitons that we think could help your business reach its' goals. We develop a customized management strategy for each of our clients to ensure desired results.</p>
                                 </div>
                             </div>
                             <div class="row">
                                 <div>
-                                    <h2> Strategy Talk</h2>
+                                    <h2><span>3</span>   Strategy Talk</h2>
                                     <p>Once our team has developed a comprehensive marketing strategy for you we will meet again to discuss if you would like to move forward with Centra, or in another direction. Either way you keep the marketing plan!</p>
                                 </div>
-                                <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" width="150" alt=""></img>
+                                <div class="hexagon">
+                                    < img src="https://images.pexels.com/photos/3184317/pexels-photo-3184317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width="650" alt="hexagon"></img>
+                                </div>
                             </div>
                             <div class="row">
-                            <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" width="150" alt=""></img>
+                                <div class="hexagon">
+                                    <img src="https://images.pexels.com/photos/5716030/pexels-photo-5716030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width="650" alt="hexagon"></img>
+                                </div>
                                 <div>
-                                    <h2> Full Rollout</h2>
+                                    <h2><span>4</span>   Full Rollout</h2>
                                     <p>This is where we will implement our comprehensive plan with a focus on prompt, strategic execution and the eventual goal of scalable growth. This will afford you the ability to redirect your attention to other critical aspects of your business and focus on what matters.</p>
                                 </div>
                             </div>
                             <div class="row">
                                 <div>
-                                    <h2>Optimization and Growth</h2>
+                                    <h2><span>5</span>   Optimization and Growth</h2>
                                     <p>Our digital marketing team will evaluate your progress daily throughout the entire process and utilize that information to drive even greater successes for you and your business. Our goal is to provide a service that allows our clients to focus on other aspects of life and completely offload digital ad management.</p>
                                 </div>
-                                <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" width="150" alt=""></img>
+                                <div class="hexagon">
+                                    <img src="https://images.pexels.com/photos/5583965/pexels-photo-5583965.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width="650"></img>
+                                </div>
                             </div>
                         </div>
                     </div>
                     
                 </div>
+                <div class="get-specfic">
+                    <h2>Any <mark>Questions?</mark></h2>
+                    <h1>Who specifically does Centra speacialize in helping?</h1>
+                    <span>Centra speacialized in assisting shopify based stores that want to grow their online traffic and overall revanue. We aim to provide the highest standard service possible to our clients ensuring ease of mind and desired results.</span>
+                    <h1>How much should I be making before contacting?</h1>
+                    <span>While we prefer our clients to be making over $20,000 a month in revanue, it really depends on each individual business' needs and what we are able and willing to provide based on the information that we gather. However Centra representitives are always happy to meet with a potential client to answer any question or see if you and your business are a right fit for us at this time.</span>
+                    <h1>What kind of results should I expect to see?</h1>
+                    <span> Whlie every client is unique here at Centra we like to aim high and think big! With this in mind our teams shoot to average 2.5 to 4 ROAS with the eventual goal of doubling or even tripling your monthly revanue.</span>
+                    <h1>How does reporting work?</h1>
+                    <span>Once you have signed on with us and gone through the onboarding process we take care of the rest. Your campaigns will immeadietly go into deployment and you will recieve a bi-weekly report of your metrics that will tell you exactly where your ad dollars are going and what we are accomlishing for you.</span>
+                    <h1>How do I get started?</h1>
+                    <span>Click the contact button below and then fill out the form! Excited to speak with you soon!</span>
                 
+                </div>
                 <div class="work-with-us">
                     <h1>Work With Us.</h1> 
                     <btn class="contact-now-btn">
